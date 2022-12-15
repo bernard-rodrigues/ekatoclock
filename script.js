0c1779b82e6702bcc1944dfeb002446d5b7ef1a8
Bernard Rodrigues <bernard.rodrigues@outlook.com>
2022-12-15 14:32:36 -0300

    Range Clock

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
+let milliseconds_in=0, milliseconds_out=0, milliseconds_per_day_of_work=0;
+
+$('#in').mask('00:00', {reverse: true});
+$('#out').mask('00:00', {reverse: true});
+
+function rangeSubmit(){
+    if($('#in').val().length >= 4 && $('#out').val().length >= 4){
+        if($('#in').val()[1] == ':' && Number($('#in').val()[2]) < 6){
+            milliseconds_in = (Number($('#in').val()[0])*3600 + Number($('#in').val().slice(2,4))*60) * 1000
+        }
+        else if(Number($('#in').val()[0]) < 3 && Number($('#in').val()[3]) < 6){
+            milliseconds_in = (Number($('#in').val().slice(0,2))*3600 + Number($('#in').val().slice(3,5))*60) * 1000
+            
+        }else{
+            alert('Dica: Utilize horário no formato 24h')
+        }
+
+        if($('#out').val()[1] == ':' && Number($('#out').val()[2]) < 6){
+            milliseconds_out = (Number($('#out').val()[0])*3600 + Number($('#out').val().slice(2,4))*60) * 1000
+        }
+        else if(Number($('#out').val()[0]) < 3 && Number($('#out').val()[3]) < 6){
+            milliseconds_out = (Number($('#out').val().slice(0,2))*3600 + Number($('#out').val().slice(3,5))*60) * 1000
+            
+        }else{
+            alert('Dica: Utilize horário no formato 24h')
+        }
+    }else{
+        alert('Dica: Utilize horário no formato 24h')
+    }
+    
+    if(milliseconds_in > milliseconds_out){
+        milliseconds_out += 24*3600*1000
+    }
+
+    milliseconds_per_day_of_work = milliseconds_out - milliseconds_in
+    console.log(milliseconds_per_day_of_work)
+}
+
 function updateTime() {
     const d = new Date();
+    
+    // COMMON CLOCK
     document.getElementById('hour').innerText = String(d.getHours())
     d.getMinutes() < 10
         ? document.getElementById('minute').innerText = '0' + String(d.getMinutes())
@@ -13,10 +53,11 @@ function updateTime() {
     document.getElementById('minutes-pointer').style.transform = `rotate(${d.getMinutes() * 6}deg)`;
     document.getElementById('hours-pointer').style.transform = `rotate(${(d.getHours() % 12) * 5 * 6}deg)`;
 
-    const miliseconds_per_day = 24 * 3600 * 1000
-    const miliseconds = (d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds()) * 1000 + d.getMilliseconds()
+    // CENTI CLOCK
+    const milliseconds_per_day = 24 * 3600 * 1000
+    const milliseconds = (d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds()) * 1000 + d.getMilliseconds()
 
-    const percentage = miliseconds / miliseconds_per_day
+    const percentage = milliseconds / milliseconds_per_day
 
     const centihours = Math.floor(percentage * 100) % 100
     const centiminutes = Math.floor(percentage * 10000) % 100
@@ -34,7 +75,29 @@ function updateTime() {
     document.getElementById('centi-minutes-pointer').style.transform = `rotate(${centiminutes * 3.6}deg)`;
     document.getElementById('centi-hours-pointer').style.transform = `rotate(${centihours * 3.6}deg)`;
 
-    document.getElementById('percentage').innerText = `${centihours},${Math.floor(centiminutes/10)}%`
+    // RANGE CLOCK
+    if(milliseconds_in != 0 && milliseconds_out != 0 && milliseconds_out > milliseconds){
+        const range_percentage = (milliseconds-milliseconds_in)/milliseconds_per_day_of_work
+        
+        const rangehours = Math.floor(range_percentage * 100) % 100
+        const rangeminutes = Math.floor(range_percentage * 10000) % 100
+        const rangeseconds = (range_percentage * 1000000) % 100
+        
+        document.getElementById('range-hour').innerText = String(rangehours)
+        rangeminutes < 10
+            ? document.getElementById('range-minute').innerText = '0' + String(rangeminutes)
+            : document.getElementById('range-minute').innerText = String(rangeminutes)
+        rangeseconds < 10
+            ? document.getElementById('range-second').innerText = '0' + String(Math.floor(rangeseconds))
+            : document.getElementById('range-second').innerText = String(Math.floor(rangeseconds))
+    
+        document.getElementById('range-seconds-pointer').style.transform = `rotate(${((range_percentage * 1000000) % 100) * 3.6}deg)`;
+        document.getElementById('range-minutes-pointer').style.transform = `rotate(${rangeminutes * 3.6}deg)`;
+        document.getElementById('range-hours-pointer').style.transform = `rotate(${rangehours * 3.6}deg)`;
+    }
+
+
+    document.getElementById('percentage').innerText = `${centihours},${Math.floor(centiminutes/10)}% do dia`
     document.getElementById('overlayer').style.width = `${100-percentage*100}%`
 }
 
